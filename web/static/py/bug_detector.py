28eb0dc75271336aa12b79afb6dabd4bf88f0144
Pavel Sagulenko <pavsag@gmail.com>
2016-06-29 20:48:53 +0200

    some minor changes in hte bug-detector

--- web/static/py/bug_detector.py
+++ web/static/py/bug_detector.py
@@ -4,61 +4,65 @@ import os,sys,copy,json
 from Bio import Phylo, AlignIO
 import treetime
 
+import matplotlib.pyplot as plt
+plt.ion()
+plt.show()
+
 #  set the configuration parameters
-reuse_branch_len=False  # Do we trust the branch lengths of the tree given? 
-infer_gtr=False  # Infer GTR model from the data given (Use default if False)?
+reuse_branch_len=True  # Do we trust the branch lengths of the tree given?
+infer_gtr=True  # Infer GTR model from the data given (Use default if False)?
 reroot = True  # Find the root node which maximizes the molecular clock correlation?
 
-use_mu=False  # Should we use the mutation rate (if False, it will be inferred from the molecular clock)? 
+use_mu=False  # Should we use the mutation rate (if False, it will be inferred from the molecular clock)?
 mu=None  #  #mutations per years per position
 resolve_poly=True  # Try to resolve multiple mergers
 coalescent = False  # Run the coalescent model (takes care about polytomies resolution)
 Tc = 0.01  #  coalescent distance (%difference )
-relax_mu = False  #  Relax mutation rate? 
+relax_mu = False  #  Relax mutation rate?
 slack = 0.0  # How much we allow for variation between parents and children (from 0 to 1)
-coupling = 0.0  # How much we allow for variation between sister branches (from 0 to 1) 
+coupling = 0.0  # How much we allow for variation between sister branches (from 0 to 1)
 
 def parse_config_dic(root):
     pass
 
 if __name__=="__main__":
 
-    
+
     if len(sys.argv) < 2 :
         root = "./"
     else:
         root = sys.argv[1]
-    
+
     # get the user's configuration parameters
     parse_config_dic(root)
-    
+
     # specify filenames
     nwk = os.path.join(root, "in_tree.nwk")
     aln = os.path.join(root, "in_aln.fasta")
     meta = os.path.join(root, "in_meta.csv")
-    
+
     gtr = treetime.GTR.standard() # always start from standard J-C model
     tt = treetime.treetime_from_newick(gtr, nwk)  # load tree
     treetime.set_seqs_to_leaves(tt, AlignIO.read(aln, 'fasta'))  # assign sequences
-    treetime.read_metadata(tt, meta)  # load meta data 
+    treetime.read_metadata(tt, meta)  # load meta data
 
     ##  Pipeline
     if not reuse_branch_len:
         tt.optimize_seq_and_branch_len(False, True)
-    
+
     if reroot:
         # This function does the following:
-        # 1. re-root 
+        # 1. re-root
         # 2. infer GTR model if needed
         # 3. ancestral inference
         # 4. init time constraints
-        tt.reroot_to_best_root(infer_gtr=infer_gtr) # we can 
+        tt.reroot_to_best_root(infer_gtr=infer_gtr) # we can
 
-    elif infer_gtr: # 
+    elif infer_gtr: #
         tt.infer_gtr()  # will make first shot in ancestral state reconstruction
-        tt.init_date_constraints(slope=mu, ancestral_inference=True) 
+        tt.init_date_constraints(slope=mu, ancestral_inference=True)
     else:
-        # just initialize  the objects needed to run the TreeTime-ML, 
+        # just initialize  the objects needed to run the TreeTime-ML,
         # and infer ancestral sequences
         tt.init_date_constraints(slope=mu, ancestral_inference=True)
 
@@ -74,7 +78,7 @@ if __name__=="__main__":
     else:
         pass  # do nothing
 
-    if relax_mu:  # Relaxed molecular clock 
+    if relax_mu:  # Relaxed molecular clock
         tt.relaxed_clock(slack, coupling)
 
 
