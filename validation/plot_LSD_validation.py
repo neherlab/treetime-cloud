461ce786642969ec282a047646e8bf55e840e83f
Pavel Sagulenko <pavel.sagulenko@tuebingen.mpg.de>
2017-05-30 10:37:51 +0200

    Adapted the plot_LSD_validation style

--- validation/plot_LSD_validation.py
+++ validation/plot_LSD_validation.py
@@ -2,13 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import argparse
 
+from plot_defaults import *
 
 parser = argparse.ArgumentParser(
         description="Run treetime on simulated data used to test LSD in To et al")
 parser.add_argument('--tree', required = True, type = str,  help ="Which tree to use, one of 750_3_25, 750_11_10, 995_11_10, 995_3_25")
 
-args = parse.parse()
+args = parser.parse_args()
 
 data_set = args.tree
 data = pd.read_csv('LSD_validation_data/Simulation_results_%s.csv'%data_set)
@@ -53,24 +55,38 @@ for rate_type in ["strict", "relaxed"]:
     bl_factor = 1.0 #np.exp(4.*TT_data[label].mean(axis=0)[2])
     tree_type = "PhyML"
     methods = set([x for x in method_map.values() if len(x) and x[-1]!='*'])
-    tmp = {'tt':(TT_data[label][:,0]*bl_factor-rate)/rate,
+    tmp = {'TT':(TT_data[label][:,0]*bl_factor-rate)/rate,
             'BSMC':(rates[(rate_type, "BSMC", "True\ntopology")]-rate)/rate}
     tmp.update({m:(rates[(rate_type, m, tree_type)]-rate)/rate for m in methods if (rate_type, m, tree_type) in rates})
     df = pd.DataFrame(tmp)
 
-    plt.figure()
-    plt.title('Evolution model: %s'%rate_type)
-    plt.hlines(0, -1, len(df), lw=3)
-    sns.violinplot(data=df)
+    fig = plt.figure(figsize=onecolumn_figsize)
+    axes = fig.add_subplot(111)
+    axes.hlines(0, -1, len(df), lw=3)
+    sns.violinplot(df)
+    #plt.figure()
+    axes.set_title('Evolution model: %s'%rate_type)
+    #plt.hlines(0, -1, len(df), lw=3)
+    #sns.violinplot(df)
     #sns.stripplot(data=df, jitter=True)
-    plt.ylabel('relative rate error')
+    plt.ylabel('relative rate error', fontsize=label_fs)
+    for tick_label in axes.get_xticklabels():
+            tick_label.set_fontsize(label_fs)
+    for tick_label in axes.get_yticklabels():
+            tick_label.set_fontsize(tick_fs)
 
-    tmp = {'tt':TT_data[label][:,1],
+    tmp = {'TT':TT_data[label][:,1],
             'BSMC':TMRCAs[(rate_type, "BSMC", "True\ntopology")]}
     tmp.update({m:TMRCAs[(rate_type, m, tree_type)] for m in methods if (rate_type, m, tree_type) in TMRCAs})
     df = pd.DataFrame(tmp)
 
-    plt.figure()
-    plt.hlines(0, -1, len(df), lw=3)
-    sns.violinplot(data=df)
-    plt.ylabel('TMRCA error [$\mathrm{Years}$]')
+    fig = plt.figure(figsize=onecolumn_figsize)
+    axes = fig.add_subplot(111)
+    axes.hlines(0, -1, len(df), lw=3)
+    sns.violinplot(df)
+    axes.set_ylabel('TMRCA error [$\mathrm{Years}$]', fontsize=label_fs)
+    for tick_label in axes.get_xticklabels():
+            tick_label.set_fontsize(label_fs)
+    for tick_label in axes.get_yticklabels():
+            tick_label.set_fontsize(tick_fs)
+
