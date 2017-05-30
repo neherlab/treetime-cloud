c2af597351cac8681da1f664489d0e8a86209eec
Pavel Sagulenko <pavel.sagulenko@tuebingen.mpg.de>
2017-05-30 12:48:09 +0200

    finished the deploy script

--- web/deploy.py
+++ web/deploy.py
@@ -3,18 +3,28 @@ Web server deploy script
 """
 
 import os
+import subprocess
 
-command = 'scp'
+
 host = 'treetime_test'
 host_location = '/var/www/treetime_web'
-
+treetime_admin = 'root'
+treetime_user = 'ttime'
 
 if __name__ == '__main__':
+    # first, copy all necessary files to the remote host
+    os.system('scp -r ./examples/ ./static/ ./templates/ ./treetime.wsgi ./treetime_server.py {}@{}:{}/'.format(treetime_admin, host, host_location))
 
-    #os.system('{} -r {}:{}/examples/ ./examples/'.format(command, host, host_location))
-    #os.system('{} -r {}:{}/static/ ./static/'.format(command, host, host_location))
-    #os.system('{} -r {}:{}/templates/ ./templates/'.format(command, host, host_location))
-    os.system('{} ./treetime.wsgi {}:{}'.format(command, host, host_location))
-    os.system('{} ./treetime_server.py {}:{}'.format(command, host, host_location))
+    # check if the sessions folder is in place
+    if not 'sessions' in subprocess.check_output('ssh -t {}@{}  "ls {}"'.format(treetime_admin, host, host_location), shell=True):
+        os.system('ssh -t {}@{}  "mkdir {}/sessions"'.format(treetime_admin, host, host_location))
+        os.system('ssh -t {}@{}  "chown -R {}:{} {}/sessions"'.format(treetime_admin, host, treetime_user, treetime_user, host_location))
+
+    #change permissions of the wsgi script:
+    os.system('ssh -t {}@{}  "chown {}:{} {}/treetime.wsgi"'.format(treetime_admin, host, treetime_user, treetime_user, host_location))
+
+
+
+
 
 
