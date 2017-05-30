0d110b104d4775a175dcc7d39d6bbb6263e0b911
Pavel Sagulenko <pavel.sagulenko@tuebingen.mpg.de>
2017-05-30 12:50:20 +0200

    few more comments

--- web/deploy.py
+++ web/deploy.py
@@ -5,17 +5,17 @@ Web server deploy script
 import os
 import subprocess
 
-
-host = 'treetime_test'
-host_location = '/var/www/treetime_web'
+host = 'treetime_test' # destination server
+host_location = '/var/www/treetime_web' # location in the destination server
 treetime_admin = 'root'
 treetime_user = 'ttime'
 
 if __name__ == '__main__':
+
     # first, copy all necessary files to the remote host
     os.system('scp -r ./examples/ ./static/ ./templates/ ./treetime.wsgi ./treetime_server.py {}@{}:{}/'.format(treetime_admin, host, host_location))
 
-    # check if the sessions folder is in place
+    # check if the sessions folder is in place. If not, create one and set the correct permissions
     if not 'sessions' in subprocess.check_output('ssh -t {}@{}  "ls {}"'.format(treetime_admin, host, host_location), shell=True):
         os.system('ssh -t {}@{}  "mkdir {}/sessions"'.format(treetime_admin, host, host_location))
         os.system('ssh -t {}@{}  "chown -R {}:{} {}/sessions"'.format(treetime_admin, host, treetime_user, treetime_user, host_location))
