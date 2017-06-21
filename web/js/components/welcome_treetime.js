fc99519d5480e7768aca24a1104f02901c8a1ea7
Richard Neher <richard.neher@unibas.ch>
2017-06-21 19:44:41 +0200

    changed upload button behavior

--- web/js/components/welcome_treetime.js
+++ web/js/components/welcome_treetime.js
@@ -83,7 +83,7 @@ var PanelFiles = React.createClass({
                     <Col xs={6} md={4} id="welcome_col_upload_tree" className="grid-treetime-col-right" >
                         <span className="btn btn-primary btn-file btn-file-treetime" id="btn-1">
                             Newick
-                            <input type="file" disabled={this.props.TreeTimeConfig.build_tree} onChange={this.props.uploadTreeFile}/>
+                            <input type="file" onChange={this.props.uploadTreeFile}/>
                         </span>
                         {this.props.appState.tree_filename}
                         <Checkbox
