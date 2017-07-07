58e1c1df164447ef9d074faf248d20a5e2f8c830
Pavel Sagulenko <pavel.sagulenko@tuebingen.mpg.de>
2017-07-07 10:12:25 +0200

    small refactor ongenerate simulated data run

--- validation/generate_simulated_dataset_run.py
+++ validation/generate_simulated_dataset_run.py
@@ -10,7 +10,7 @@ if  __name__ == '__main__':
     GENERATE_SIMULATED_DATA = False
     RUN_TREETIME = True
     RUN_LSD = True
-    RUN_BEAST = True
+    RUN_BEAST = False
 
     sys.stderr.write ("  ".join(sys.argv) + "\n")
 
@@ -27,31 +27,27 @@ if  __name__ == '__main__':
     # run evolution simulation, produce basic tree and alignment
     if GENERATE_SIMULATED_DATA:
         print ("Running FFpopSim")
-        basename = utils_sim.run_ffpopsim_simulation(L, N, SAMPLE_VOL, SAMPLE_NUM, SAMPLE_FREQ, MU, res_dir, suffix, optimize_branch_len=True)
+        basename = utils_sim.run_ffpopsim_simulation(L, N, SAMPLE_VOL, SAMPLE_NUM,
+                                SAMPLE_FREQ, MU, res_dir, suffix, optimize_branch_len=True)
 
         print ("Running FastTree reconstruction")
         utils_sim.reconstruct_fasttree(basename, optimize_branch_len=False)
 
     else:  # use previously generated data
-        basename = "FFpopSim_L{}_N{}_Ns{}_Ts{}_Nv{}_Mu{}".format(str(L), str(N),
+        label = "FFpopSim_L{}_N{}_Ns{}_Ts{}_Nv{}_Mu{}".format(str(L), str(N),
                  str(SAMPLE_NUM), str(SAMPLE_FREQ), str(SAMPLE_VOL), str(MU))
 
-        basename = os.path.join(res_dir, basename)
+        basename = os.path.join(res_dir, label)
         if suffix != "":
              basename = basename + "_" + suffix
 
     if RUN_TREETIME:
-        # run treetime for original tree:
-        print ("Running TreeTime, original tree")
-        outfile = outfile_prefix + "_treetime_res.csv"
-        utils_sim.run_treetime(basename, outfile, fasttree=False, failed=None, max_iter=3, use_input_branch_length=True)
 
-        # run treetime for FastTree-reconstructed tree:
-        print ("Running TreeTime, FastTree tree")
-        outfile = outfile_prefix + "_treetime_fasttree_res.csv"
-        utils_sim.run_treetime(basename, outfile, fasttree=True, failed=None, max_iter=3, use_input_branch_length=True)
-
-        #utils_sim.run_treetime(basename, outfile_prefix + "_treetime_fasttree_res_use_input_branch_false.csv", fasttree=True, failed=None, max_iter=3, use_input_branch_length=False)
+        for fasttree in [True, False]:
+            # run treetime for original tree:
+            print ("Running TreeTime, {} tree".format ("FastTree" if fasttree else "Original"))
+            outfile = outfile_prefix + "_treetime_{}res.csv".format("fasttree_" if fasttree else "")
+            utils_sim.run_treetime(basename, outfile, fasttree=fasttree, failed=None, max_iter=3, use_input_branch_length=True)
 
     if RUN_LSD:
         # run LSD for original tree:
@@ -62,20 +58,14 @@ if  __name__ == '__main__':
         except:
             pass
 
-        # file to store formatted results of LSD run
-        print ("Running LSd, original tree")
-        outfile = outfile_prefix + "_lsd_res.csv"
-        treefile = basename + ".opt.nwk"
-        lsd_res_file = os.path.join(outfile_prefix + "_lsd", os.path.split(basename)[-1])
-        lsd_dates_file = os.path.join(outfile_prefix + "_lsd", os.path.split(basename)[-1] + ".lsd_dates.txt")
-        utils_sim.run_lsd(treefile, lsd_dates_file, lsd_res_file, outfile)
-
-        # run LSD for fast-tree tree:
-        print ("Running LSD, FastTree tree")
-        outfile = outfile_prefix + "_lsd_fasttree_res.csv"
-        treefile = basename + ".ft.nwk"
-        lsd_res_file = os.path.join(outfile_prefix + "_lsd", os.path.split(basename)[-1] + "_fasttree")
-        utils_sim.run_lsd(treefile, lsd_dates_file, lsd_res_file, outfile)
+        for fasttree in [True, False]:
+            # file to store formatted results of LSD run
+            print ("Running LSd, {} tree".format("FastTree" if fasttree else "Original"))
+            outfile = outfile_prefix + "_lsd_{}res.csv".format("fasttree_" if fasttree else "")
+            treefile = basename + ".ft.nwk" if fasttree else basename + ".opt.nwk"
+            lsd_res_file = os.path.join(outfile_prefix + "_lsd", os.path.split(basename)[-1]) + "{}".format("_fasttree" if fasttree else "")
+            lsd_dates_file = os.path.join(outfile_prefix + "_lsd", os.path.split(basename)[-1] + ".lsd_dates.txt")
+            utils_sim.run_lsd(treefile, lsd_dates_file, lsd_res_file, outfile)
 
     if RUN_BEAST:
         # run BEAST for the original tree:
