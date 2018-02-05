045b7261fa93a15131dfb97b783e1a2b7b8d30db
Richard Neher <richard.neher@unibas.ch>
2018-02-05 11:46:59 +0100

    fix version of bootstrap

--- web/treetime_server.py
+++ web/treetime_server.py
@@ -15,7 +15,7 @@ ALLOWED_EXTENSIONS = ['fasta', 'nwk', 'csv', 'png', 'jpg']
 
 # html theme is controlled by the server
 #html_theme_css = "http://bootswatch.com/flatly/bootstrap.css"
-html_theme_css = "https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"
+html_theme_css = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 
 # set the server directories
 dn = os.path.dirname(os.path.abspath(__file__))
