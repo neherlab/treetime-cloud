24bf74986c0ef8d2dfdeaa0f296430fc46b3cfa0
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-02-09 12:48:05 +0100

    feat(worker): receive task and download input files

--- worker/src/tree_time_process.py
+++ worker/src/tree_time_process.py
@@ -554,46 +554,58 @@ def run_treeanc(root, webconfig):
     ttw.run_treeanc()
 
 
-if __name__=="__main__":
-    root = '../data/'
-    import  tree_time_config
+if __name__ == "__main__":
+    root = "../data/"
+    import tree_time_config
+
     cfg = tree_time_config.treetime_webconfig
 
     import pika
+    import json
+
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='treetime-dev-taskqueue')
+        pika.ConnectionParameters(host="treetime-dev-taskqueue")
     )
 
     channel = connection.channel()
 
-    channel.queue_declare(queue='tasks')
+    channel.queue_declare(queue="tasks")
 
     def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
+        try:
+            task = json.loads(body)["data"]
+            task_id = task["taskId"]
+            input_filepaths = task["inputFilepaths"]
+            print(input_filepaths)
+        except:
+            # TODO: report failure back to api
+            sys.stderr.write(f"Error: task {task_id}: data is corrupt. Aborting.")
 
         import boto3
         from botocore.client import Config
+
         session = boto3.session.Session()
         s3 = boto3.client(
-          service_name='s3',
-          endpoint_url='http://treetime-dev-filestore:9000',
-          aws_access_key_id='minioadmin',
-          aws_secret_access_key='minioadmin',
-          config=Config(signature_version='s3v4')
+            service_name="s3",
+            endpoint_url="http://treetime-dev-filestore:9000",
+            aws_access_key_id="minioadmin",
+            aws_secret_access_key="minioadmin",
+            config=Config(signature_version="s3v4"),
         )
-        with open('../data/h3n2_na_20.fasta', 'wb') as f:
-            s3.download_fileobj('treetime', 'a048c7a8-0cec-45e2-a7ec-b4499b1f289e/h3n2_na_20.fasta', f)
+
+        def download_from_s3(path: str):
+            out_path = os.path.join("..", "data", path)
+            os.makedirs(os.path.dirname(out_path), exist_ok=True)
+            s3.download_file("treetime", path, out_path)
+
+        for _, filepath in input_filepaths.items():
+            download_from_s3(filepath)
 
         # ttw = TreeTimeWeb(root, cfg)
         # ttw.run()
 
-    channel.basic_consume(
-        queue='tasks', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue="tasks", on_message_callback=callback, auto_ack=True)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
+    print(" [*] Waiting for messages. To exit press CTRL+C")
     channel.start_consuming()
-
-
-
-
 
