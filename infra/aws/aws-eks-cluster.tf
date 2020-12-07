b8f07afa535c42bdc52121e98ecdc8e939a59910
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-12-07 04:23:03 +0100

    chore: configure runner node taints
    We add node *taints*:
    
    node_type=runner:NoSchedule
    node_type=runner:NoExecute
    
    This will ensure that only pods with the corresponding *tolerations* cab be scheduled and executed on these nodes. We will later add these tolerations to the pod specs.
    
    We also add hint labels for cluster autoscaler so that it takes into accounts when deciding which nodes to scale.

--- infra/aws/aws-eks-cluster.tf
+++ infra/aws/aws-eks-cluster.tf
@@ -2,34 +2,64 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "13.2.1"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.18"
-  subnets         = module.vpc.private_subnets
-
-  vpc_id = module.vpc.vpc_id
-
-  manage_aws_auth = false
-
+  cluster_name       = local.cluster_name
+  cluster_version    = "1.18"
+  subnets            = module.vpc.private_subnets
+  vpc_id             = module.vpc.vpc_id
+  manage_aws_auth    = true
   config_output_path = "kubeconfig/kubeconfig.yaml"
 
   worker_groups = [
     {
-      name                          = "worker-group-1"
+      name                          = "worker-group-basic"
       instance_type                 = "t2.small"
-      asg_desired_capacity          = 1
+      spot_price                    = ""
+      asg_min_size                  = 1
+      asg_max_size                  = 1
+      asg_recreate_on_change        = true
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
       labels = {
-        "worker-type" : "worker-t2.small"
+        "node_type" : "basic"
       }
     },
     {
-      name                          = "worker-group-2"
+      name                          = "worker-group-runner"
       instance_type                 = "t2.medium"
+      asg_min_size                  = 1
+      asg_max_size                  = 4
+      asg_recreate_on_change        = true
+      default_cooldown              = null
+      protect_from_scale_in         = false
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
       labels = {
-        "worker-type" : "worker-t2.medium"
+        "node_type" : "runner"
       }
+      tags = [
+        {
+          "key"                 = "k8s.io/cluster-autoscaler/enabled"
+          "value"               = "true"
+          "propagate_at_launch" = "false"
+        },
+        {
+          "key"                 = "k8s.io/cluster-autoscaler/${local.cluster_name}"
+          "value"               = "true"
+          "propagate_at_launch" = "false"
+        },
+        {
+          "key"                 = "k8s.io/cluster-autoscaler/node-template/taint/node_type"
+          "value"               = "runner:NoSchedule"
+          "propagate_at_launch" = "false"
+        },
+        {
+          "key"                 = "k8s.io/cluster-autoscaler/node-template/taint/node_type"
+          "value"               = "runner:NoExecute"
+          "propagate_at_launch" = "false"
+        },
+      ]
+      kubelet_extra_args = join(" ", [
+        "--node-labels=cluster_name=${local.cluster_name},node_type=runner",
+        "--register-with-taints node_type=runner:NoSchedule,node_type=runner:NoExecute"
+      ])
     },
   ]
 
@@ -45,4 +75,11 @@ data "aws_eks_cluster" "cluster" {
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
 }
